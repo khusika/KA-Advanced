morogoku/MTweaks-KernelAdiutorMOD

Add Spectrum settings
Signed-off-by: morogoku <morogoku@hotmail.com>

app/src/main/java/com/moro/mtweaks/activities/MainActivity.java
@@ -61,6 +61,7 @@ import com.moro.mtweaks.utils.kernel.ksm.KSM;
 import com.moro.mtweaks.utils.kernel.misc.Vibration;
 import com.moro.mtweaks.utils.kernel.screen.Screen;
 import com.moro.mtweaks.utils.kernel.sound.Sound;
+import com.moro.mtweaks.utils.kernel.spectrum.Spectrum;
 import com.moro.mtweaks.utils.kernel.thermal.Thermal;
 import com.moro.mtweaks.utils.kernel.wake.Wake;
 import com.moro.mtweaks.utils.root.RootUtils;
@@ -108,6 +109,10 @@ public class MainActivity extends BaseActivity {
         if (!Utils.existFile("/data/.mtweaks")) {
             RootUtils.runCommand("mkdir /data/.mtweaks");
         }
+
+        //Initialice profile Sharedpreference
+        int prof = Utils.strToInt(Spectrum.getProfile());
+        AppSettings.saveInt("spectrum_profile", prof, this);
 
         setContentView(R.layout.activity_main);
 

app/src/main/java/com/moro/mtweaks/activities/NavigationActivity.java
@@ -62,6 +62,7 @@ import com.moro.mtweaks.fragments.kernel.WakelockFragment;
 import com.moro.mtweaks.fragments.kernel.MiscFragment;
 import com.moro.mtweaks.fragments.kernel.ScreenFragment;
 import com.moro.mtweaks.fragments.kernel.SoundFragment;
+import com.moro.mtweaks.fragments.kernel.SpectrumFragment;
 import com.moro.mtweaks.fragments.kernel.ThermalFragment;
 import com.moro.mtweaks.fragments.kernel.VMFragment;
 import com.moro.mtweaks.fragments.kernel.WakeFragment;
@@ -101,6 +102,7 @@ import com.moro.mtweaks.utils.kernel.led.LED;
 import com.moro.mtweaks.utils.kernel.lmk.LMK;
 import com.moro.mtweaks.utils.kernel.screen.Screen;
 import com.moro.mtweaks.utils.kernel.sound.Sound;
+import com.moro.mtweaks.utils.kernel.spectrum.Spectrum;
 import com.moro.mtweaks.utils.kernel.thermal.Thermal;
 import com.moro.mtweaks.utils.kernel.wake.Wake;
 import com.moro.mtweaks.utils.kernel.wakelock.Wakelock;
@@ -216,6 +218,9 @@ public class NavigationActivity extends BaseActivity
         }
         if (Sound.getInstance().supported()) {
             mFragments.add(new NavigationActivity.NavigationFragment(R.string.sound, SoundFragment.class, R.drawable.ic_music));
+        }
+        if (Spectrum.supported()) {
+            mFragments.add(new NavigationActivity.NavigationFragment(R.string.spectrum, SpectrumFragment.class, R.drawable.ic_spectrum_logo));
         }
         if (Battery.getInstance(this).supported()) {
             mFragments.add(new NavigationActivity.NavigationFragment(R.string.battery, BatteryFragment.class, R.drawable.ic_battery));

app/src/main/java/com/moro/mtweaks/utils/kernel/spectrum/ProfileTile.java
@@ -1,0 +1,129 @@
+package com.moro.mtweaks.utils.kernel.spectrum;
+
+import android.annotation.TargetApi;
+import android.graphics.drawable.Icon;
+import android.os.Build;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import com.moro.mtweaks.R;
+import com.moro.mtweaks.utils.AppSettings;
+import com.moro.mtweaks.utils.Utils;
+
+/**
+ * Created by Morogoku on 01/08/2017.
+ */
+
+@TargetApi(Build.VERSION_CODES.N)
+public class ProfileTile extends TileService {
+
+    private static final String SERVICE_STATUS_FLAG = "serviceStatus";
+    private boolean click = false;
+
+    @Override
+    public void onStartListening() {
+        resetTileStatus();
+    }
+
+    @Override
+    public void onClick() {
+        updateTile();
+    }
+
+    private void updateTile() {
+        Tile tile = this.getQsTile();
+        boolean isActive = getServiceStatus();
+        Icon newIcon;
+        String newLabel;
+        int newState;
+
+        // Update tile and set profile
+        if (isActive && click) {
+            newLabel = "Spectrum Gaming";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_game);
+            newState = Tile.STATE_ACTIVE;
+            click = false;
+            Spectrum.setProfile(3);
+            AppSettings.saveInt("spectrum_profile", 3, getApplicationContext());
+        } else if (!isActive && click) {
+            newLabel = "Spectrum Battery";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_battery);
+            newState = Tile.STATE_ACTIVE;
+            click = true;
+            Spectrum.setProfile(2);
+            AppSettings.saveInt("spectrum_profile", 2, getApplicationContext());
+        } else if (isActive && !click){
+            newLabel = "Spectrum Performance";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_performance);
+            newState = Tile.STATE_ACTIVE;
+            click = true;
+            Spectrum.setProfile(1);
+            AppSettings.saveInt("spectrum_profile", 1, getApplicationContext());
+        } else {
+            newLabel = "Spectrum Balance";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_balanced);
+            newState = Tile.STATE_ACTIVE;
+            click = false;
+            Spectrum.setProfile(0);
+            AppSettings.saveInt("spectrum_profile", 0, getApplicationContext());
+        }
+
+        // Change the UI of the tile.
+        tile.setLabel(newLabel);
+        tile.setIcon(newIcon);
+        tile.setState(newState);
+        tile.updateTile();
+    }
+
+    private boolean getServiceStatus() {
+
+        boolean isActive = AppSettings.getBoolean(SERVICE_STATUS_FLAG, false, getApplicationContext());
+        isActive = !isActive;
+
+        AppSettings.saveBoolean(SERVICE_STATUS_FLAG, isActive, getApplicationContext());
+
+        return isActive;
+    }
+
+    private void resetTileStatus() {
+        int profile = Utils.strToInt(Spectrum.getProfile());
+        Tile tile = this.getQsTile();
+        Icon newIcon;
+        String newLabel;
+        int newState;
+
+        // Update tile
+        if (profile == 3){
+            newLabel = "Spectrum Gaming";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_game);
+            newState = Tile.STATE_ACTIVE;
+            click = false;
+        } else if (profile == 2){
+            newLabel = "Spectrum Battery";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_battery);
+            newState = Tile.STATE_ACTIVE;
+            click = true;
+        } else if (profile == 1){
+            newLabel = "Spectrum Performance";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_performance);
+            newState = Tile.STATE_ACTIVE;
+            click = true;
+        } else if (profile == 0){
+            newLabel = "Spectrum Balance";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_balanced);
+            newState = Tile.STATE_ACTIVE;
+            click = false;
+        } else {
+            newLabel = "Spectrum Custom";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_logo);
+            newState = Tile.STATE_ACTIVE;
+            click = false;
+        }
+
+        // Change the UI of the tile.
+        tile.setLabel(newLabel);
+        tile.setIcon(newIcon);
+        tile.setState(newState);
+        tile.updateTile();
+    }
+}

app/src/main/java/com/moro/mtweaks/utils/kernel/spectrum/Spectrum.java
@@ -1,0 +1,45 @@
+package com.moro.mtweaks.utils.kernel.spectrum;
+
+import android.os.AsyncTask;
+
+import com.moro.mtweaks.utils.Utils;
+import com.moro.mtweaks.utils.root.RootUtils;
+
+/**
+ * Created by Morogoku on 28/07/2017.
+ */
+
+public class Spectrum {
+
+    private static final String SPECTRUM = "/init.spectrum.sh";
+
+
+    public static String getProfile(){
+        return RootUtils.runCommand("getprop persist.spectrum.profile");
+    }
+
+    // Method that interprets a profile and sets it
+    public static void setProfile(int profile) {
+        int numProfiles = 3;
+        if (profile > numProfiles || profile < 0) {
+            setProp(0);
+        } else {
+            setProp(profile);
+        }
+    }
+
+    // Method that sets system property
+    private static void setProp(final int profile) {
+        new AsyncTask<Object, Object, Void>() {
+            @Override
+            protected Void doInBackground(Object... params) {
+                RootUtils.runCommand("setprop persist.spectrum.profile " + profile);
+                return null;
+            }
+        }.execute();
+    }
+
+    public static boolean supported() {
+        return Utils.existFile(SPECTRUM);
+    }
+}

khusika/KA-Advanced

Signed-off-by: khusika <khusikadhamar@gmail.com>

app/src/main/java/com/grarak/kerneladiutor/activities/MainActivity.java
@@ -52,6 +52,7 @@ import com.grarak.kerneladiutor.utils.kernel.ksm.KSM;
 import com.grarak.kerneladiutor.utils.kernel.misc.Vibration;
 import com.grarak.kerneladiutor.utils.kernel.screen.Screen;
 import com.grarak.kerneladiutor.utils.kernel.sound.Sound;
+import com.grarak.kerneladiutor.utils.kernel.spectrum.Spectrum;
 import com.grarak.kerneladiutor.utils.kernel.thermal.Thermal;
 import com.grarak.kerneladiutor.utils.kernel.wake.Wake;
 import com.grarak.kerneladiutor.utils.root.RootUtils;
@@ -70,6 +71,9 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Initialice profile Sharedpreference
+        int prof = Utils.strToInt(Spectrum.getProfile());
+        AppSettings.saveInt("spectrum_profile", prof, this);
         setContentView(R.layout.activity_main);
 
         View splashBackground = findViewById(R.id.splash_background);

app/src/main/java/com/grarak/kerneladiutor/activities/NavigationActivity.java
@@ -59,6 +59,7 @@ import com.grarak.kerneladiutor.fragments.kernel.LMKFragment;
 import com.grarak.kerneladiutor.fragments.kernel.MiscFragment;
 import com.grarak.kerneladiutor.fragments.kernel.ScreenFragment;
 import com.grarak.kerneladiutor.fragments.kernel.SoundFragment;
+import com.grarak.kerneladiutor.fragments.kernel.SpectrumFragment;
 import com.grarak.kerneladiutor.fragments.kernel.ThermalFragment;
 import com.grarak.kerneladiutor.fragments.kernel.VMFragment;
 import com.grarak.kerneladiutor.fragments.kernel.WakeFragment;
@@ -95,6 +96,7 @@ import com.grarak.kerneladiutor.utils.kernel.led.LED;
 import com.grarak.kerneladiutor.utils.kernel.lmk.LMK;
 import com.grarak.kerneladiutor.utils.kernel.screen.Screen;
 import com.grarak.kerneladiutor.utils.kernel.sound.Sound;
+import com.grarak.kerneladiutor.utils.kernel.spectrum.Spectrum;
 import com.grarak.kerneladiutor.utils.kernel.thermal.Thermal;
 import com.grarak.kerneladiutor.utils.kernel.wake.Wake;
 import com.grarak.kerneladiutor.utils.root.RootUtils;
@@ -198,6 +200,9 @@ public class NavigationActivity extends BaseActivity
         }
         if (Sound.getInstance().supported()) {
             mFragments.add(new NavigationActivity.NavigationFragment(R.string.sound, SoundFragment.class, R.drawable.ic_music));
+        }
+        if (Spectrum.supported()) {
+            mFragments.add(new NavigationActivity.NavigationFragment(R.string.spectrum, SpectrumFragment.class, R.drawable.ic_spectrum_logo));
         }
         if (Battery.getInstance(this).supported()) {
             mFragments.add(new NavigationActivity.NavigationFragment(R.string.battery, BatteryFragment.class, R.drawable.ic_battery));

app/src/main/java/com/grarak/kerneladiutor/utils/kernel/spectrum/ProfileTile.java
@@ -1,0 +1,129 @@
+package com.grarak.kerneladiutor.utils.kernel.spectrum;
+
+import android.annotation.TargetApi;
+import android.graphics.drawable.Icon;
+import android.os.Build;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import com.grarak.kerneladiutor.R;
+import com.grarak.kerneladiutor.utils.AppSettings;
+import com.grarak.kerneladiutor.utils.Utils;
+
+/**
+ * Created by Morogoku on 01/08/2017.
+ */
+
+@TargetApi(Build.VERSION_CODES.N)
+public class ProfileTile extends TileService {
+
+    private static final String SERVICE_STATUS_FLAG = "serviceStatus";
+    private boolean click = false;
+
+    @Override
+    public void onStartListening() {
+        resetTileStatus();
+    }
+
+    @Override
+    public void onClick() {
+        updateTile();
+    }
+
+    private void updateTile() {
+        Tile tile = this.getQsTile();
+        boolean isActive = getServiceStatus();
+        Icon newIcon;
+        String newLabel;
+        int newState;
+
+        // Update tile and set profile
+        if (isActive && click) {
+            newLabel = "Spectrum Gaming";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_game);
+            newState = Tile.STATE_ACTIVE;
+            click = false;
+            Spectrum.setProfile(3);
+            AppSettings.saveInt("spectrum_profile", 3, getApplicationContext());
+        } else if (!isActive && click) {
+            newLabel = "Spectrum Battery";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_battery);
+            newState = Tile.STATE_ACTIVE;
+            click = true;
+            Spectrum.setProfile(2);
+            AppSettings.saveInt("spectrum_profile", 2, getApplicationContext());
+        } else if (isActive && !click){
+            newLabel = "Spectrum Performance";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_performance);
+            newState = Tile.STATE_ACTIVE;
+            click = true;
+            Spectrum.setProfile(1);
+            AppSettings.saveInt("spectrum_profile", 1, getApplicationContext());
+        } else {
+            newLabel = "Spectrum Balance";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_balanced);
+            newState = Tile.STATE_ACTIVE;
+            click = false;
+            Spectrum.setProfile(0);
+            AppSettings.saveInt("spectrum_profile", 0, getApplicationContext());
+        }
+
+        // Change the UI of the tile.
+        tile.setLabel(newLabel);
+        tile.setIcon(newIcon);
+        tile.setState(newState);
+        tile.updateTile();
+    }
+
+    private boolean getServiceStatus() {
+
+        boolean isActive = AppSettings.getBoolean(SERVICE_STATUS_FLAG, false, getApplicationContext());
+        isActive = !isActive;
+
+        AppSettings.saveBoolean(SERVICE_STATUS_FLAG, isActive, getApplicationContext());
+
+        return isActive;
+    }
+
+    private void resetTileStatus() {
+        int profile = Utils.strToInt(Spectrum.getProfile());
+        Tile tile = this.getQsTile();
+        Icon newIcon;
+        String newLabel;
+        int newState;
+
+        // Update tile
+        if (profile == 3){
+            newLabel = "Spectrum Gaming";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_game);
+            newState = Tile.STATE_ACTIVE;
+            click = false;
+        } else if (profile == 2){
+            newLabel = "Spectrum Battery";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_battery);
+            newState = Tile.STATE_ACTIVE;
+            click = true;
+        } else if (profile == 1){
+            newLabel = "Spectrum Performance";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_performance);
+            newState = Tile.STATE_ACTIVE;
+            click = true;
+        } else if (profile == 0){
+            newLabel = "Spectrum Balance";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_balanced);
+            newState = Tile.STATE_ACTIVE;
+            click = false;
+        } else {
+            newLabel = "Spectrum Custom";
+            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_spectrum_tile_logo);
+            newState = Tile.STATE_ACTIVE;
+            click = false;
+        }
+
+        // Change the UI of the tile.
+        tile.setLabel(newLabel);
+        tile.setIcon(newIcon);
+        tile.setState(newState);
+        tile.updateTile();
+    }
+}

app/src/main/java/com/grarak/kerneladiutor/utils/kernel/spectrum/Spectrum.java
@@ -1,0 +1,45 @@
+package com.grarak.kerneladiutor.utils.kernel.spectrum;
+
+import android.os.AsyncTask;
+
+import com.grarak.kerneladiutor.utils.Utils;
+import com.grarak.kerneladiutor.utils.root.RootUtils;
+
+/**
+ * Created by Morogoku on 28/07/2017.
+ */
+
+public class Spectrum {
+
+    private static final String SPECTRUM = "/init.spectrum.sh";
+
+
+    public static String getProfile(){
+        return RootUtils.runCommand("getprop persist.spectrum.profile");
+    }
+
+    // Method that interprets a profile and sets it
+    public static void setProfile(int profile) {
+        int numProfiles = 3;
+        if (profile > numProfiles || profile < 0) {
+            setProp(0);
+        } else {
+            setProp(profile);
+        }
+    }
+
+    // Method that sets system property
+    private static void setProp(final int profile) {
+        new AsyncTask<Object, Object, Void>() {
+            @Override
+            protected Void doInBackground(Object... params) {
+                RootUtils.runCommand("setprop persist.spectrum.profile " + profile);
+                return null;
+            }
+        }.execute();
+    }
+
+    public static boolean supported() {
+        return Utils.existFile(SPECTRUM);
+    }
+}