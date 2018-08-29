morogoku/MTweaks-KernelAdiutorMOD

Wireguard VPN version

app/src/main/java/com/moro/mtweaks/fragments/kernel/MiscFragment.java
@@ -33,6 +33,7 @@ import com.moro.mtweaks.utils.kernel.misc.Selinux;
 import com.moro.mtweaks.utils.kernel.misc.Vibration;
 import com.moro.mtweaks.utils.kernel.misc.Wakelocks;
 import com.moro.mtweaks.views.recyclerview.CardView;
+import com.moro.mtweaks.views.recyclerview.DescriptionView;
 import com.moro.mtweaks.views.recyclerview.GenericSelectView;
 import com.moro.mtweaks.views.recyclerview.RecyclerViewItem;
 import com.moro.mtweaks.views.recyclerview.SeekBarView;
@@ -348,6 +349,14 @@ public class MiscFragment extends RecyclerViewFragment {
         });
 
         networkCard.addItem(hostname);
+
+        if (Misc.hasWireguard()) {
+            DescriptionView wireguard = new DescriptionView();
+            wireguard.setTitle(getString(R.string.wireguard_title));
+            wireguard.setSummary(getString(R.string.version) + ": " + Misc.getWireguard());
+
+            networkCard.addItem(wireguard);
+        }
 
         items.add(networkCard);
     }

khusika/KA-Advanced

Signed-off-by: khusika <khusikadhamar@gmail.com>

app/src/main/java/com/grarak/kerneladiutor/fragments/kernel/MiscFragment.java
@@ -30,6 +30,7 @@ import com.grarak.kerneladiutor.utils.kernel.misc.PowerSuspend;
 import com.grarak.kerneladiutor.utils.kernel.misc.Vibration;
 import com.grarak.kerneladiutor.utils.kernel.misc.Wakelocks;
 import com.grarak.kerneladiutor.views.recyclerview.CardView;
+import com.grarak.kerneladiutor.views.recyclerview.DescriptionView;
 import com.grarak.kerneladiutor.views.recyclerview.GenericSelectView;
 import com.grarak.kerneladiutor.views.recyclerview.RecyclerViewItem;
 import com.grarak.kerneladiutor.views.recyclerview.SeekBarView;
@@ -251,6 +252,14 @@ public class MiscFragment extends RecyclerViewFragment {
                 -> mMisc.setHostname(value, getActivity()));
 
         networkCard.addItem(hostname);
+
+        if (Misc.hasWireguard()) {
+            DescriptionView wireguard = new DescriptionView();
+            wireguard.setTitle(getString(R.string.wireguard_title));
+            wireguard.setSummary(getString(R.string.version) + ": " + Misc.getWireguard());
+
+            networkCard.addItem(wireguard);
+        }
 
         items.add(networkCard);
     }